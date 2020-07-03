e7d16a289101cf2487ee1e464c9503ed76055df5
gokul <gokulakrishnan.312@gmail.com>
2020-07-04 01:56:01 +0530

    Updates

---
+++ precache-manifest.366bc70fae04942f2b86b0c0c56e9fb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50302889f2b358e3d7fe5d30d6053bd0",
+    "url": "/reactjs-ToDoList-2.0/index.html"
+  },
+  {
+    "revision": "7ca935dc842df1b14956",
+    "url": "/reactjs-ToDoList-2.0/static/css/main.092dee2b.chunk.css"
+  },
+  {
+    "revision": "7f62067ed7d868372cc6",
+    "url": "/reactjs-ToDoList-2.0/static/js/2.c3b8c4ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-ToDoList-2.0/static/js/2.c3b8c4ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca935dc842df1b14956",
+    "url": "/reactjs-ToDoList-2.0/static/js/main.bcd9c45c.chunk.js"
+  },
+  {
+    "revision": "29ed5266090194d26f05",
+    "url": "/reactjs-ToDoList-2.0/static/js/runtime-main.4996061e.js"
+  }
+]);
